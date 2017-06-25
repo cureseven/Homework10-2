4571eff768a973353627fb26a9eb32ae45037ca6
小笠原 奈々 <shiningcureseven@gmail.com>
2017-06-26 01:10:14 +0900

    修正

--- Sample1.java
+++ Sample1.java
@@ -1,40 +1,62 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileWriter;
 
 class Dengonban{
-  static String filename = "dengonban.txt";
-  void read(){
-    try{
-    File file = new File(dengonban.txt);
-    FileWriter filewriter = new FileWriter(file);
-    BufferedWriter bw = new BufferedWriter(filewriter);
-    PrintWriter pw = new PrintWriter(bw);
-  }catch(IOException e){
-    System.out.println("メッセージはありません．");
-  }
-}
-  void write()throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String str = br.readLine();
-    pw.println(str);
-  }
-  }
+	static String filename = "dengonban.txt";
+	void read(){
+		try{
+		    BufferedReader br = new BufferedReader(new FileReader(filename));
+		    try {
+		            String line = br.readLine();
+		            System.out.println(line.toUpperCase());
+		    } finally{
+		        br.close();
+		    }
+		    }catch (IOException e) {
+			System.out.println("メッセージはありません．");
+		 }
+	}
+	void write() throws IOException{
+			BufferedReader br =
+			new BufferedReader(new InputStreamReader(System.in));
+			String str1 = br.readLine();
+			try{
+			      File file = new File(filename);
 
-class Sample1{
-  public static void main(String[] args)throws IOException{
-    Dengonban d = new Dengonban();
-    System.out.println("メッセージを読む：1，書く：2");
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int c = parseInt(br.readLine());
-    try{
-    d.read();
-    if(c == 1){
-      d.read();
-    }else if(c == 2){
-      d.write();
-    }
-  }catch(IOException e){
-    System.out.println("入出力エラーです．");
-  }
-  }
-  pw.close();
+			        FileWriter filewriter = new FileWriter(file);
+
+			        filewriter.write(str1+"\r\n");
+
+			        filewriter.close();
+			    }catch(IOException e){
+			      System.out.println(e);
+			    }
+	}
+}
+public class Sample1 {
+	public static void main(String[] args)throws IOException {
+		Dengonban d = new Dengonban();
+		System.out.println("メッセージを読む:1　書く:2");
+		try{
+			BufferedReader br =
+				new BufferedReader(new InputStreamReader(System.in));
+				String str = br.readLine();
+				int c = Integer.parseInt(str);//
+				if(c==1){
+					d.read();
+				}else if(c==2){
+					d.write();
+				}else
+				{
+					System.out.println("入出力エラーです");
+				}
+
+		}catch(IOException e){
+			System.out.println("入出力エラーです");
+		}
+	}
 }
